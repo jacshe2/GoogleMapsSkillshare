44da3757c22426dc0656676c569ecfce634d2a8b
jacshe2 <68033829+jacshe2@users.noreply.github.com>
2021-06-19 22:17:34 +0100

    Add files via upload

--- maps.js
+++ maps.js
@@ -1,29 +1,62 @@
 
+
+
 function initMap() {
-  var location = 'LE2 2ND';
-  var geocode = new google.maps.Geocoder();
+  const locationcoords = { lat: 51.500729, lng: -0.124625 };
 
-  geocode.geocode({
-    'address': location
-  }, function(results, status) {
-	//works map address
-    if (status != google.maps.GeocoderStatus.OK) {
-    	console.log("Geocode error");
-    }else{
-      var latitude = results[0].geometry.location.lat();
-      var longitude = results[0].geometry.location.lng();
-      	locationcoords = { lat: latitude, lng:  longitude };
-  		const map = new google.maps.Map(document.getElementById("map"), {
-   		zoom: 11,
-   		//center is the marker
-    	center: locationcoords,
-});
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: locationcoords,
+    zoom: 13,
+  });
 
-  //draw marker
+  const iconBase =
+  "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
+const icons = {
+ 
+  info: {
+    icon: iconBase + "info-i_maps.png",
+  },
+};
+const locations = [
+  {
+    //big ben
+    position: new google.maps.LatLng(51.500729, -0.124625),
+    type: "info",
+  },
+  {
+    //london eye
+    position: new google.maps.LatLng(51.503324, -0.119543),
+    type: "info",
+  },
+  {
+    //downing
+    position: new google.maps.LatLng(51.5034, -0.1276),
+    type: "info",
+  },
+  
+];
+
+// Create markers.
+for (let i = 0; i < locations.length; i++) {
   const marker = new google.maps.Marker({
-    position: locationcoords,
+    position: locations[i].position,
+    animation: google.maps.Animation.DROP,
+    icon: icons[locations[i].type].icon,
     map: map,
-  });    
-    }
   });
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
